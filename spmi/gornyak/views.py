d5bb6a830c458f9d91e7b9b1e01632073c2a8c22
SultanAlmaty <ser_yushkov@mail.ru>
2024-05-23 19:17:18 +0300

    Add files

--- spmi/gornyak/views.py
+++ spmi/gornyak/views.py
@@ -56,6 +56,7 @@ def event_list(request):
         'locations': locations,
     })
 
+@login_required
 def event_detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     return render(request, 'gornyak/event_detail.html', {'event': event})
